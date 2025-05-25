091defa4b7ebfd69eb2144a21ee89a661f93de01
inventech-solution <info@inventechsolution.com>
2025-05-26 03:39:44 +0530

    Update facebookConfig.php with other Ad Account

--- libs/facebookConfig.php
+++ libs/facebookConfig.php
@@ -3,6 +3,6 @@ return [
     'app_id' => '860249399560466',
     'app_secret' => '795a197cb1f59d6df11ec9461b42c2b6',
     'access_token' => 'EAAMOZAGvhCRIBO0UM0RG97Ce6W0oBHuSWZAP6X15JAC7ghPWCN0yy7FZCM8LUZAvUmD0MEMMyBOcxOf84d1EoSmDsmybaibk2BPTp1MXLAUa5ZC6voerMHY0JlYpt6nJOidTxaOzwtOT6Xy7TYqCjnzG4v8i3mZAPZClm71QLt6zNkvHO6WKeCujQ1M',
-    'ad_account_id' => 'act_984386718779769'
+    'ad_account_id' => 'act_79565875'
 ];
 ?>
